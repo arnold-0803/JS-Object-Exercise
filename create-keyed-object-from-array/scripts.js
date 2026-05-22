5828ae8d9ffd79be941f71ac536d4d445b779c76
Arnold Kiritu <arnoldkells4@gmail.com>
2026-05-22 21:00:22 +0300

    wrote the create keyed object to array code
    the create keyed object to array code, creates object with id as key and array items as values

---
+++ create-keyed-object-from-array/scripts.js
@@ -0,0 +1,18 @@
+function groupById(arr){
+  return arr.reduce((previousArr, currentArr) => {
+    previousArr[currentArr.id] = currentArr;
+
+    return previousArr;
+  }, {});
+}
+
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+
+console.log(usersById);
